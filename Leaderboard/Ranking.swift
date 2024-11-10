5d04b2d0ef16734cbdbfd3353745eace48406e22
Gordon Choi <thefirate@gmail.com>
2024-11-10 18:37:13 +0900

    모델 정의

---
+++ Leaderboard/Ranking.swift
@@ -0,0 +1,25 @@
+//
+//  Ranking.swift
+//  Leaderboard
+//
+//  Created by Gordon Choi on 11/10/24.
+//
+
+import Foundation
+
+protocol Ranking: Hashable {
+    var userId: String { get }
+    var nickname: String { get }
+}
+
+struct ScoreRanking: Ranking {
+    let userId: String
+    let nickname: String
+    let score: Int
+}
+
+struct TimeRanking: Ranking {
+    let userId: String
+    let nickname: String
+    let timestamp: String
+}
