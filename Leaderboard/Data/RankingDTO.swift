a5a18914901b9f11dfd26589071fd4c460bd9d69
Gordon Choi <thefirate@gmail.com>
2024-11-24 18:18:31 +0900

    의도적으로 에러 발생시킨 부분 제거

--- Leaderboard/Data/RankingDTO.swift
+++ Leaderboard/Data/RankingDTO.swift
@@ -29,27 +29,3 @@ struct RankingDTO: Codable {
         return TimeRanking(userId: userId, nickname: nickname, seconds: seconds)
     }
 }
-
-// MARK: - Experiment for decoding error
-struct ViceRankingResponse: Codable {
-    let data: [ViceRankingDTO]?
-}
-
-struct ViceRankingDTO: Codable {
-    let userId: String
-    let nickname: String
-    let score: Int?
-    let seconds: Int?
-    
-    func toScoreRanking() -> ScoreRanking? {
-        guard let score else { return nil }
-        
-        return ScoreRanking(userId: userId, nickname: nickname, score: score)
-    }
-
-    func toTimeRanking() -> TimeRanking? {
-        guard let seconds else { return nil }
-        
-        return TimeRanking(userId: userId, nickname: nickname, seconds: Double(seconds))
-    }
-}
